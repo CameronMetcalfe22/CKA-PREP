9fb57a251983f7115a94a49eb38796902171f8d1
Manas-KIRAD <91735513+BrimstoneXmk@users.noreply.github.com>
2025-10-27 07:48:42 +0530

    namespace was missing in helm template command
    Added -n argocd

--- Question-1/SolutionNotes.bash
+++ Question-1/SolutionNotes.bash
@@ -4,7 +4,7 @@ helm repo add argocd https://argoproj.github.io/argo-helm
 helm repo list
 
 # Step two get the template using the parameters given
-helm template argocd argo/argo-cd --version 7.7.3 --set crds.install=false --namespace argocd > /root/argo-helm.yaml
+helm template argocd argo/argo-cd -n argocd --version 7.7.3 --set crds.install=false --namespace argocd > /root/argo-helm.yaml
 
 #Step three verfiy
 cat /root/argo-helm.yaml
